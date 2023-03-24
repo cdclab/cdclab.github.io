29861281bb12c723d4ebe413b7f2138df8d82ff5
cdclab <125351288+cdclab@users.noreply.github.com>
2023-03-24 17:01:25 +0800

    Fix: extra line break

--- js/ideas.js
+++ js/ideas.js
@@ -15,12 +15,12 @@ export default {
 		    </div>
 	    </section>
 	    <section class="body-container">
-		    <section class="wrapper-horizontal">
+		    <article class="wrapper-horizontal">
 		    	<h2>How is self-representation developed?</h2>
-		    	<p>Our understanding of the self is accumulated through the eyes of others: We interact with the people around us and assimilate evaluative feedback from them regarding different aspects of the self. We learn not only about who we are but also how to relate to others to satisfy needs. As early intimate relationships appear to be the most influential in these learning processes, people who have been subjected to early adverse relational experiences (e.g., emotional, physical, and sexual maltreatment) may develop a dysfunctional self-representation due to persistent invalidating and often inconsistent feedback. They may include fragmented self-aspects, confusion between self and others, and overgeneralized negative self-evaluation. <br><br></p>
+		    	<p>Our understanding of the self is accumulated through the eyes of others: We interact with the people around us and assimilate evaluative feedback from them regarding different aspects of the self. We learn not only about who we are but also how to relate to others to satisfy needs. As early intimate relationships appear to be the most influential in these learning processes, people who have been subjected to early adverse relational experiences (e.g., emotional, physical, and sexual maltreatment) may develop a dysfunctional self-representation due to persistent invalidating and often inconsistent feedback. They may include fragmented self-aspects, confusion between self and others, and overgeneralized negative self-evaluation.</p>
 
 		    	<h2>Dysfunctional self-representation and trauma spectrum disorders</h2>
-		    	<p>We posit that people with trauma spectrum disorders, including but not limited to dissociative disorders, borderline personality disorder, and complex post-traumatic stress disorder, are characterized by different forms of dysfunctional self-representation that can be revealed in a variety of basic cognitive functions. To the extent that many interpersonal and intrapersonal functions build on top of these cognitive functions, people with trauma spectrum disorders are found to have deficits in the social domains such as empathy, feedback processing, and emotion regulation under relational stress. <br><br></p>
+		    	<p>We posit that people with trauma spectrum disorders, including but not limited to dissociative disorders, borderline personality disorder, and complex post-traumatic stress disorder, are characterized by different forms of dysfunctional self-representation that can be revealed in a variety of basic cognitive functions. To the extent that many interpersonal and intrapersonal functions build on top of these cognitive functions, people with trauma spectrum disorders are found to have deficits in the social domains such as empathy, feedback processing, and emotion regulation under relational stress.</p>
 
 				<h2>Our approaches</h2>
 		    	<p>Gaining insights from clinical practice about the underlying structure of sociocognitive functioning described above, we experiment with them in both clinical and non-clinical populations. We adopt a combination of methods to capture responses at different levels, which includes physiological measures, behavioral tests, observations, and introspective reports. Below shows our research framework and examples of relevant topics and phenomena that we are interested in:</p>
@@ -33,7 +33,7 @@ export default {
 			    	<p class="toggleBtn highlight" @click.prevent="$emit('clickPage', './?page=project-2')"><i class="fa fa-chevron-right toggle-icon"></i>Distorted Self-Referential Memory in Trauma-Related Disorders</p>
 			    	<p class="toggleBtn highlight" @click.prevent="$emit('clickPage', './?page=project-3')"><i class="fa fa-chevron-right toggle-icon"></i>Biased Self-Evaluation in Trauma-Related Disorders</p>
 		    	</div>
-		    </section>
+		    </article>
 	    </section>
     </main>`
 }
