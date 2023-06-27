a3afbbd0d2ab496b0177632cc85ce3ff24bf7251
cdclab <125351288+cdclab@users.noreply.github.com>
2023-06-27 11:41:46 +0800

    Update photos

--- js/home.js
+++ js/home.js
@@ -59,10 +59,12 @@ export default {
 						</div>
 						<div class="flex-60">
 							<splide :options="{ type: 'fade', rewind: true, speed: 2000, arrows: false, drag: false, autoplay: true, interval: 4000, pauseOnHover: false, pauseOnFocus: false, lazyLoad: 'sequential' }" aria-label="Services">
+								<splide-slide><img data-splide-lazy="./img/home/service-photo-2023-06.jpg" width="636" height="358" alt=""><span class="splide-description">Jun 2023 - HKCS Workshop</span></splide-slide>
 								<splide-slide><img data-splide-lazy="./img/home/service-photo-2023-05.jpg" width="636" height="358" alt=""><span class="splide-description">May 2023 - Chiayi, Taiwan</span></splide-slide>
 								<splide-slide><img data-splide-lazy="./img/home/service-photo-2022-12-1.jpg" width="636" height="358" alt=""><span class="splide-description">Dec 2022 - BOKSS Workshop</span></splide-slide>
 								<splide-slide><img data-splide-lazy="./img/home/service-photo-2022-12-3.jpg" width="636" height="358" alt=""><span class="splide-description">Dec 2022 - BOKSS Workshop</span></splide-slide>
 								<splide-slide><img data-splide-lazy="./img/home/service-photo-2022-12-2.jpg" width="636" height="358" alt=""><span class="splide-description">Dec 2022 - HKCS Workshop</span></splide-slide>
+								<splide-slide><img data-splide-lazy="./img/home/service-photo-2022-08.jpg" width="636" height="358" alt=""><span class="splide-description">Aug 2022 - BOKSS Supervision</span></splide-slide>
 								<splide-slide><img data-splide-lazy="./img/home/service-photo-2022-06.jpg" width="636" height="358" alt=""><span class="splide-description">Jun 2022 - NTU-CUHK Mix-mode Seminar</span></splide-slide>
 								<splide-slide><img data-splide-lazy="./img/home/service-photo-2020-11.jpg" width="636" height="358" alt=""><span class="splide-description">Nov 2020 - HKCS Workshop</span></splide-slide>
 								<splide-slide><img data-splide-lazy="./img/home/service-photo-2019-05.jpg" width="636" height="358" alt=""><span class="splide-description">May 2019 - SWD Workshop</span></splide-slide>
